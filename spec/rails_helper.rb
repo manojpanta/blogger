13301e18e62ee73fb8cd56989f9c5e840b45b9fa
Manoj Panta <manojpanta95@gmail.com>
2018-05-10 15:50:51 -0600

    add setup for testing with rspes and shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,6 +56,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     # Choose a test framework:
