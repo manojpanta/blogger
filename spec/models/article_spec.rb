0ca9f78545452b1c441c88de5b67b850c3b03cbd
Manoj Panta <manojpanta95@gmail.com>
2018-05-10 15:55:05 -0600

    add article spec wwith validations

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,8 @@
+require 'rails-helper'
+
+describe Article, type: :model do
+  describe 'validations' do
+    it{should vlaidate_presenceOf(:title)}
+    it{should vlaidate_presenceOf(:body)}
+  end
+end
